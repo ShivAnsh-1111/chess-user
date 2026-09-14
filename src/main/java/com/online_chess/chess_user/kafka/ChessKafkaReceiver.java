201facea758ff1dff9f42373484ca67166b5ca6e
ShivAnsh-1111 <147432347+ShivAnsh-1111@users.noreply.github.com>
2026-09-15 00:42:17 +0530

    CU-001: Local oracle + init run

--- src/main/java/com/online_chess/chess_user/kafka/ChessKafkaReceiver.java
+++ src/main/java/com/online_chess/chess_user/kafka/ChessKafkaReceiver.java
@@ -1,16 +1,17 @@
 package com.online_chess.chess_user.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.stereotype.Service;
 
-import lombok.extern.java.Log;
-
 @Service
-@Log
 public class ChessKafkaReceiver {
+
+    private static final Logger log = LoggerFactory.getLogger(ChessKafkaReceiver.class);
 	
 	/*
 	 * @Autowired
